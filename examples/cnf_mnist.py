d8f246e0f204641d5da67b2f4d49ad3c612fd459
Jannik Wagner <jannik.wagner@students.uni-mainz.de>
2022-05-19 13:28:05 +0200

    cnf_mnist

--- examples/cnf_mnist.py
+++ examples/cnf_mnist.py
@@ -126,3 +126,415 @@ class HyperNetwork(nn.Module):
 
         B = params[3 * self.blocksize:].reshape(self.width, 1, self.yolo_dim)
         return [W, B, U]
+
+################################
+import copy
+
+def unsqueeze(input, upscale_factor=2):
+    '''
+    [:, C*r^2, H, W] -> [:, C, H*r, W*r]
+    '''
+    batch_size, in_channels, in_height, in_width = input.size()
+    out_channels = in_channels // (upscale_factor**2)
+
+    out_height = in_height * upscale_factor
+    out_width = in_width * upscale_factor
+
+    input_view = input.contiguous().view(batch_size, out_channels, upscale_factor, upscale_factor, in_height, in_width)
+
+    output = input_view.permute(0, 1, 4, 2, 5, 3).contiguous()
+    return output.view(batch_size, out_channels, out_height, out_width)
+
+
+def squeeze(input, downscale_factor=2):
+    '''
+    [:, C, H*r, W*r] -> [:, C*r^2, H, W]
+    '''
+    batch_size, in_channels, in_height, in_width = input.size()
+    out_channels = in_channels * (downscale_factor**2)
+
+    out_height = in_height // downscale_factor
+    out_width = in_width // downscale_factor
+
+    input_view = input.contiguous().view(
+        batch_size, in_channels, out_height, downscale_factor, out_width, downscale_factor
+    )
+
+    output = input_view.permute(0, 1, 3, 5, 2, 4).contiguous()
+    return output.view(batch_size, out_channels, out_height, out_width)
+
+class ConcatConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, ksize=3, stride=1, padding=0, dilation=1, groups=1, bias=True, transpose=False):
+        super(ConcatConv2d, self).__init__()
+        module = nn.ConvTranspose2d if transpose else nn.Conv2d
+        self._layer = module(
+            dim_in + 1, dim_out, kernel_size=ksize, stride=stride, padding=padding, dilation=dilation, groups=groups,
+            bias=bias
+        )
+
+    def forward(self, t, x):
+        tt = torch.ones_like(x[:, :1, :, :]) * t
+        ttx = torch.cat([tt, x], 1)
+        return self._layer(ttx)
+
+
+NONLINEARITIES = {
+    "tanh": nn.Tanh(),
+    "relu": nn.ReLU(),
+    "softplus": nn.Softplus(),
+    "elu": nn.ELU()
+}
+
+class ODEnet(nn.Module):
+    """
+    Helper class to make neural nets for use in continuous normalizing flows
+    """
+
+    def __init__(
+        self, hidden_dims, input_shape, strides, conv, layer_type="concat", nonlinearity="softplus", num_squeeze=0
+    ):
+        super(ODEnet, self).__init__()
+        self.num_squeeze = num_squeeze
+        assert len(strides) == len(hidden_dims) + 1
+        base_layer = ConcatConv2d
+        # build layers and add them
+        layers = []
+        activation_fns = []
+        hidden_shape = input_shape
+
+        for dim_out, stride in zip(hidden_dims + (input_shape[0],), strides):
+            if stride is None:
+                layer_kwargs = {}
+            elif stride == 1:
+                layer_kwargs = {"ksize": 3, "stride": 1, "padding": 1, "transpose": False}
+            elif stride == 2:
+                layer_kwargs = {"ksize": 4, "stride": 2, "padding": 1, "transpose": False}
+            elif stride == -2:
+                layer_kwargs = {"ksize": 4, "stride": 2, "padding": 1, "transpose": True}
+            else:
+                raise ValueError('Unsupported stride: {}'.format(stride))
+
+            layer = base_layer(hidden_shape[0], dim_out, **layer_kwargs)
+            layers.append(layer)
+            activation_fns.append(NONLINEARITIES[nonlinearity])
+
+            hidden_shape = list(copy.copy(hidden_shape))
+            hidden_shape[0] = dim_out
+            if stride == 2:
+                hidden_shape[1], hidden_shape[2] = hidden_shape[1] // 2, hidden_shape[2] // 2
+            elif stride == -2:
+                hidden_shape[1], hidden_shape[2] = hidden_shape[1] * 2, hidden_shape[2] * 2
+
+        self.layers = nn.ModuleList(layers)
+        self.activation_fns = nn.ModuleList(activation_fns[:-1])
+
+    def forward(self, t, y):
+        dx = y
+        # squeeze
+        for _ in range(self.num_squeeze):
+            dx = squeeze(dx, 2)
+        for l, layer in enumerate(self.layers):
+            dx = layer(t, dx)
+            # if not last layer, use nonlinearity
+            if l < len(self.layers) - 1:
+                dx = self.activation_fns[l](dx)
+        # unsqueeze
+        for _ in range(self.num_squeeze):
+            dx = unsqueeze(dx, 2)
+        return dx
+
+
+def divergence_bf(dx, y, **unused_kwargs):
+    sum_diag = 0.
+    for i in range(y.shape[1]):
+        sum_diag += torch.autograd.grad(dx[:, i].sum(), y, create_graph=True)[0].contiguous()[:, i].contiguous()
+    return sum_diag.contiguous()
+
+def divergence_approx(f, y, e=None):
+    e_dzdx = torch.autograd.grad(f, y, e, create_graph=True)[0]
+    e_dzdx_e = e_dzdx * e
+    approx_tr_dzdx = e_dzdx_e.view(y.shape[0], -1).sum(dim=1)
+    return approx_tr_dzdx
+
+def sample_gaussian_like(y):
+    return torch.randn_like(y)
+
+class ODEfunc(nn.Module):
+
+    def __init__(self, diffeq, divergence_fn="approximate", residual=False, rademacher=False):
+        super(ODEfunc, self).__init__()
+        assert divergence_fn in ("brute_force", "approximate")
+
+        # self.diffeq = diffeq_layers.wrappers.diffeq_wrapper(diffeq)
+        self.diffeq = diffeq
+        self.residual = residual
+        
+        if divergence_fn == "brute_force":
+            self.divergence_fn = divergence_bf
+        elif divergence_fn == "approximate":
+            self.divergence_fn = divergence_approx
+
+        self.register_buffer("_num_evals", torch.tensor(0.))
+
+    def before_odeint(self, e=None):
+        self._e = e
+        self._num_evals.fill_(0)
+
+    def num_evals(self):
+        return self._num_evals.item()
+
+    def forward(self, t, states):
+        assert len(states) >= 2
+        y = states[0]
+
+        # increment num evals
+        self._num_evals += 1
+
+        # convert to tensor
+        t = torch.tensor(t).type_as(y)
+        batchsize = y.shape[0]
+
+        # Sample and fix the noise.
+        if self._e is None:
+            self._e = sample_gaussian_like(y)
+
+        with torch.set_grad_enabled(True):
+            y.requires_grad_(True)
+            t.requires_grad_(True)
+            for s_ in states[2:]:
+                s_.requires_grad_(True)
+            dy = self.diffeq(t, y, *states[2:])
+            # Hack for 2D data to use brute force divergence computation.
+            if not self.training and dy.view(dy.shape[0], -1).shape[1] == 2:
+                divergence = divergence_bf(dy, y).view(batchsize, 1)
+            else:
+                divergence = self.divergence_fn(dy, y, e=self._e).view(batchsize, 1)
+        if self.residual:
+            dy = dy - y
+            divergence -= torch.ones_like(divergence) * torch.tensor(np.prod(y.shape[1:]), dtype=torch.float32
+                                                                     ).to(divergence)
+        return tuple([dy, -divergence] + [torch.zeros_like(s_).requires_grad_(True) for s_ in states[2:]])
+
+class CNF(nn.Module):
+    def __init__(self, odefunc, T=1.0, train_T=False, regularization_fns=None, solver='dopri5', atol=1e-5, rtol=1e-5):
+        super(CNF, self).__init__()
+        if train_T:
+            self.register_parameter("sqrt_end_time", nn.Parameter(torch.sqrt(torch.tensor(T))))
+        else:
+            self.register_buffer("sqrt_end_time", torch.sqrt(torch.tensor(T)))
+
+        nreg = 0
+        if regularization_fns is not None:
+            odefunc = RegularizedODEfunc(odefunc, regularization_fns)
+            nreg = len(regularization_fns)
+        self.odefunc = odefunc
+        self.nreg = nreg
+        self.regularization_states = None
+        self.solver = solver
+        self.atol = atol
+        self.rtol = rtol
+        self.test_solver = solver
+        self.test_atol = atol
+        self.test_rtol = rtol
+        self.solver_options = {}
+
+    def forward(self, z, logpz=None, integration_times=None, reverse=False):
+
+        if logpz is None:
+            _logpz = torch.zeros(z.shape[0], 1).to(z)
+        else:
+            _logpz = logpz
+
+        if integration_times is None:
+            integration_times = torch.tensor([0.0, self.sqrt_end_time * self.sqrt_end_time]).to(z)
+        if reverse:
+            integration_times = _flip(integration_times, 0)
+
+        # Refresh the odefunc statistics.
+        self.odefunc.before_odeint()
+
+        # Add regularization states.
+        reg_states = tuple(torch.tensor(0).to(z) for _ in range(self.nreg))
+
+        if self.training:
+            state_t = odeint(
+                self.odefunc,
+                (z, _logpz) + reg_states,
+                integration_times.to(z),
+                atol=self.atol,
+                rtol=self.rtol,
+                method=self.solver,
+                options=self.solver_options,
+            )
+        else:
+            state_t = odeint(
+                self.odefunc,
+                (z, _logpz),
+                integration_times.to(z),
+                atol=self.test_atol,
+                rtol=self.test_rtol,
+                method=self.test_solver,
+            )
+
+        if len(integration_times) == 2:
+            state_t = tuple(s[1] for s in state_t)
+
+        z_t, logpz_t = state_t[:2]
+        self.regularization_states = state_t[2:]
+
+        if logpz is not None:
+            return z_t, logpz_t
+        else:
+            return z_t
+
+    def get_regularization_states(self):
+        reg_states = self.regularization_states
+        self.regularization_states = None
+        return reg_states
+
+    def num_evals(self):
+        return self.odefunc._num_evals.item()
+
+
+
+def _flip(x, dim):
+    indices = [slice(None)] * x.dim()
+    indices[dim] = torch.arange(x.size(dim) - 1, -1, -1, dtype=torch.long, device=x.device)
+    return x[tuple(indices)]
+
+class RegularizedODEfunc(nn.Module):
+    def __init__(self, odefunc, regularization_fns):
+        super(RegularizedODEfunc, self).__init__()
+        self.odefunc = odefunc
+        self.regularization_fns = regularization_fns
+
+    def before_odeint(self, *args, **kwargs):
+        self.odefunc.before_odeint(*args, **kwargs)
+
+    def forward(self, t, state):
+        class SharedContext(object):
+            pass
+
+        with torch.enable_grad():
+            x, logp = state[:2]
+            x.requires_grad_(True)
+            logp.requires_grad_(True)
+            dstate = self.odefunc(t, (x, logp))
+            if len(state) > 2:
+                dx, dlogp = dstate[:2]
+                reg_states = tuple(reg_fn(x, logp, dx, dlogp, SharedContext) for reg_fn in self.regularization_fns)
+                return dstate + reg_states
+            else:
+                return dstate
+
+    @property
+    def _num_evals(self):
+        return self.odefunc._num_evals
+
+
+def _batch_root_mean_squared(tensor):
+    tensor = tensor.view(tensor.shape[0], -1)
+    return torch.mean(torch.norm(tensor, p=2, dim=1) / tensor.shape[1]**0.5)
+
+
+def l1_regularzation_fn(x, logp, dx, dlogp, unused_context):
+    del x, logp, dlogp
+    return torch.mean(torch.abs(dx))
+
+
+def l2_regularzation_fn(x, logp, dx, dlogp, unused_context):
+    del x, logp, dlogp
+    return _batch_root_mean_squared(dx)
+
+
+def directional_l2_regularization_fn(x, logp, dx, dlogp, unused_context):
+    del logp, dlogp
+    directional_dx = torch.autograd.grad(dx, x, dx, create_graph=True)[0]
+    return _batch_root_mean_squared(directional_dx)
+
+
+def jacobian_frobenius_regularization_fn(x, logp, dx, dlogp, context):
+    del logp, dlogp
+    if hasattr(context, "jac"):
+        jac = context.jac
+    else:
+        jac = _get_minibatch_jacobian(dx, x)
+        context.jac = jac
+    return _batch_root_mean_squared(jac)
+
+
+def jacobian_diag_frobenius_regularization_fn(x, logp, dx, dlogp, context):
+    del logp, dlogp
+    if hasattr(context, "jac"):
+        jac = context.jac
+    else:
+        jac = _get_minibatch_jacobian(dx, x)
+        context.jac = jac
+    diagonal = jac.view(jac.shape[0], -1)[:, ::jac.shape[1]]  # assumes jac is minibatch square, ie. (N, M, M).
+    return _batch_root_mean_squared(diagonal)
+
+
+def jacobian_offdiag_frobenius_regularization_fn(x, logp, dx, dlogp, context):
+    del logp, dlogp
+    if hasattr(context, "jac"):
+        jac = context.jac
+    else:
+        jac = _get_minibatch_jacobian(dx, x)
+        context.jac = jac
+    diagonal = jac.view(jac.shape[0], -1)[:, ::jac.shape[1]]  # assumes jac is minibatch square, ie. (N, M, M).
+    ss_offdiag = torch.sum(jac.view(jac.shape[0], -1)**2, dim=1) - torch.sum(diagonal**2, dim=1)
+    ms_offdiag = ss_offdiag / (diagonal.shape[1] * (diagonal.shape[1] - 1))
+    return torch.mean(ms_offdiag)
+
+
+def _get_minibatch_jacobian(y, x, create_graph=False):
+    """Computes the Jacobian of y wrt x assuming minibatch-mode.
+
+    Args:
+      y: (N, ...) with a total of D_y elements in ...
+      x: (N, ...) with a total of D_x elements in ...
+    Returns:
+      The minibatch Jacobian matrix of shape (N, D_y, D_x)
+    """
+    assert y.shape[0] == x.shape[0]
+    y = y.view(y.shape[0], -1)
+
+    # Compute Jacobian row by row.
+    jac = []
+    for j in range(y.shape[1]):
+        dy_j_dx = torch.autograd.grad(y[:, j], x, torch.ones_like(y[:, j]), retain_graph=True,
+                                      create_graph=True)[0].view(x.shape[0], -1)
+        jac.append(torch.unsqueeze(dy_j_dx, 1))
+    jac = torch.cat(jac, 1)
+    return jac
+
+
+def create_model(args, data_shape, regularization_fns):
+    hidden_dims = tuple(map(int, args.dims.split(",")))
+    strides = tuple(map(int, args.strides.split(",")))
+
+    def build_cnf():
+        diffeq = ODEnet(
+            hidden_dims=hidden_dims,
+            input_shape=data_shape,
+            strides=strides,
+            conv=args.conv,
+            layer_type=args.layer_type,
+            nonlinearity=args.nonlinearity,
+        )
+        odefunc = ODEfunc(
+            diffeq=diffeq,
+            divergence_fn=args.divergence_fn,
+            residual=args.residual,
+            rademacher=args.rademacher,
+        )
+        cnf = CNF(
+            odefunc=odefunc,
+            T=args.time_length,
+            train_T=args.train_T,
+            regularization_fns=regularization_fns,
+            solver=args.solver,
+        )
+        return cnf
+    model = build_cnf()
+    return model
